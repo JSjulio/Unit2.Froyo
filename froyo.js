fbe370b488c8babf9324bbc4db74f03c95d3ef72
Isaias Julio <isaiasjulio@isaiass-air-2.mynetworksettings.com>
2023-11-05 18:50:00 -0500

    pseudocode

---
+++ froyo.js
@@ -0,0 +1,42 @@
+// The user is prompted for froyo flavors upon entering the website.
+order = prompt(
+    'List the flavors of froyo you want to order today'
+    'vanilla','vanilla',
+    );
+
+
+// The user's input string is split into an array of strings.
+// Use the  JSON.stringify tools to recieve in the input from the user and make it a function
+
+
+
+// A loop is used to iterate through the array of flavors.
+
+
+
+// An object is used to keep count of how many orders there are of each flavor.
+
+
+
+// The program correctly counts the number of each flavor in the user's input.
+
+
+
+// The logic for counting the frequencies of elements in an array is ...
+// organized into a function that returns an object.
+
+
+
+// Names are consistent and meaningful 
+
+
+
+// The console output changes depending on the user's input.
+//if else
+
+
+
+
+
+
+
